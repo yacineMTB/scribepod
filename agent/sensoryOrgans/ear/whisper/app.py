003134175dd75e4f565df4998998ccc983c849b0
yacine <yacine.bram@gmail.com>
2023-01-28 20:10:28 -0500

    Add semaphore on frontend, fail at profiling

--- agent/sensoryOrgans/ear/whisper/app.py
+++ agent/sensoryOrgans/ear/whisper/app.py
@@ -2,12 +2,18 @@ import os
 import tempfile
 import flask
 from flask import request
+# from werkzeug.middleware.profiler import ProfilerMiddleware
 from flask_cors import CORS
 import whisper
 import time
 
 app = flask.Flask(__name__)
+# use profiler middleware but with the profile directory set as the current dir
 CORS(app)
+# app = ProfilerMiddleware(app, profile_dir='.')
+app.config['PROFILE'] = True
+# app.wsgi_app = ProfilerMiddleware(app.wsgi_app, profile_dir='.')
+
 
 model = None
 def load_model():
@@ -18,10 +24,8 @@ def load_model():
 def transcribe():
     if not model:
         load_model()
-
     start_time_1 = time.time()
     temp_dir = tempfile.mkdtemp()
-
     save_path = os.path.join(temp_dir, 'temp.wav')
     print(save_path)
     wav_file = request.files['audio_data']
